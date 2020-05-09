1138b38cdd21483ab627d35035e71d618443bbb0
Erik Gúth <guthember@gmail.com>
2020-05-09 14:58:07 +0200

    First commit

---
+++ caesar/count-x-y-w.py
@@ -0,0 +1,5 @@
+ # Számoljuk össze (egy megírt program segítségével) hányszor szerepel a szövegben az X, Y és # W karakter. 
+ # Az eredmény: X_száma + Y_száma - W_száma
+
+ with open('count-x-y-w.txt') as file:
+   pass   
