0db69fbb558baacadb5e5172c5902f6797d8b01a
Erik Gúth <guthember@gmail.com>
2020-05-09 22:16:40 +0200

    Caesar finished -> poem.txt

--- caesar/ceasar.py
+++ caesar/ceasar.py
@@ -4,3 +4,30 @@
 # értéke. A feltörési feladatok megoldásához bármilyen programozási nyelvet használhatnak a
 # résztvevők.
 
+# eltolás = 8 + 4 + 4 -> 16
+
+
+shift = 16
+englishABCU = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+englishABCL = englishABCU.lower()
+
+shiftABCU = englishABCU[shift:] + englishABCU[0:shift]
+shiftABCL = shiftABCU.lower()
+
+poem = open('poem.txt','w')
+
+with open('caesar.txt') as file:
+  for line in file:
+    newLine = ""
+    line = line.strip()
+    for char in line:
+      if englishABCU.find(char) > -1:
+        newLine += englishABCU[shiftABCU.find(char)]
+      elif englishABCL.find(char) > -1:
+        newLine += englishABCL[shiftABCL.find(char)]
+      else:
+        newLine += char
+    poem.write(newLine+'\n')
+
+poem.close()
+ 
