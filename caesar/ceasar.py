da8be4c37352c4117697c62585e51b035713882f
Erik Gúth <guthember@gmail.com>
2020-05-09 16:38:32 +0200

    Caesar python program has started

---
+++ caesar/ceasar.py
@@ -0,0 +1,6 @@
+# A vers szövege több caesar.txt file-ban van lekódolva a Caesar-rejtjel segítségével.
+# A kódolás az angol ABC-t használja és eltolást értékét másik feladatok alapján kapjuk meg.
+# A többi feladat eredménye mind szám, feladatok eredményeinek összege a kódolás eltolásának
+# értéke. A feltörési feladatok megoldásához bármilyen programozási nyelvet használhatnak a
+# résztvevők.
+
